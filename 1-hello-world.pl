30cb2c930d4457e9b9401b16064fa9d83ff53a4e
Bhagyesh <dudhediyabv@gmail.com>
2017-10-10 13:15:07 +0530

    Update 1-hello-world.pl

--- 1-hello-world.pl
+++ 1-hello-world.pl
@@ -4,7 +4,7 @@ use warnings;
 
 =begin
 This is a multiline comment.
-This commen will be ignored until next =cut is encountered by interpreter
+This comment will be ignored until next =cut is encountered by interpreter
 =cut
 
 # This will print "Hello, World"
