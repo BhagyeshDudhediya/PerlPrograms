9121179250fbd167df25b792bbca3e81b48f965a
Bhagyesh Dudhediya <dudhediyabv@gmail.com>
2017-10-23 13:33:53 +0530

    Add some description to regex file.

--- 15-regex.pl
+++ 15-regex.pl
@@ -23,6 +23,22 @@ a\.c			a.c exactly
 100\s*mk		the strings 100 and mk optionally separated by any amount of white space (spaces, tabs, newlines)
 abc\b			abc when followed by a word boundary (e.g. in abc! but not in abcd)
 perl\B			perl when not followed by a word boundary (e.g. in perlert but not in perl stuff)
+
+Quantifiers in Perl: Pperl matches every character only once, but it gives flexibility to match multiple occurences of character using Quantifiers.
+1. ? - matching 0 or 1 occurences
+2. + - matching 1 or more occurences
+3. * - matching 0 or more occurences
+
+{} - used to express ranges. x{1,3} - means 1, 2 or 3 x-es.
+. (dot) - matches any one character except new-line. Eg: /c.t/ matches cat, cut, c t, c.t,...
+- (dash) - Defines range in character class. Eg: /[a-z]/ matches any character between a to z.
+^ (Caret) - matches anything except those mentioned in regex. Eg: /[^abc]/ matches any character except a, b, c.
+\d (Digit) - Matches the digits equivalent to [0-9]
+\w (word character) - Equivalent to [0-9a-zA-z_]. Matches digits, ASCII characters and underscore.
+\s (whitespace) - matches horizontal tab, carraige return, white space, new-line, form feed. Eqivalent to [\t\f\n\r ].
+\D - Negated \d.
+\W - Negated \w.
+\S - Negated \s.
 =cut
 
 $bar = "This statement contains foo";
